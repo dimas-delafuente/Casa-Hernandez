960fe79e06974d6949b6b9936830f6a7a148c996
Delaefe <dimasdelafuen@gmail.com>
2016-10-06 22:37:37 +0200

    Project Structure and Navbar Style
    Creada la organización para los ficheros.
    Desarrollado el estilo del navbar sin redireccionamiento.

--- controller/index.php
+++ controller/index.php
@@ -1,12 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Casa Hernandez</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
+<?php
 
-	
-</body>
-</html>
+date_default_timezone_set('UTC');
+
+/*
+ * This is the controller php file.
+ * It has possible passed values of $action and $category_id
+ * 
+ * 1) None - this is the inex page so show the default category ($category_id=1)
+ * 2) POST input
+ * 3) GET input
+ */
+
+//Need the files to connect to the DB and to make functions available
+require ("../model/database.php");
+require ("../model/productos_db.php");
+require ("../model/categorias_db.php");
+
+
+/* A variable 'action' is needed, it can be passed by POST, GET or not at all
+ * If not at all, then make action = default of list_products
+ */
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+	$action = filter_input(INPUT_GET, 'action');
+	if ($action == NULL) {
+		$action = 'inicio';
+	}
+}
+
+
+switch ($action) {
+	case 'inicio':
+
+	$title = "Casa Hernandez";
+	$header = "";
+	$header_title = "Inicio";
+
+	include('../view/inicio.php');
+	break;
+
+}
+?>
+
---
+++ view/navbar.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="../css/style.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+
+
+	<header role="banner">
+
+	<div class="container">
+
+		<nav class="site-nav" role="navigation">
+
+			<ul>
+				<li><a href="">Inicio</a></li>
+				<li><a href="">Empresa</a></li>
+				<li><a href="">Productos</a></li>
+				<li><a href="">Contacto</a></li>
+				<li><a href="">Ofertas</a></li>
+				<li><a href="">Jovira</a></li>
+			</ul>
+			
+		</nav>
+
+	</div>
+	
+	</header>
+
+	
+</body>
+</html>
