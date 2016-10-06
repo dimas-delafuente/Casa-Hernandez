34be2548ef1640c91eafdd58756b92a447c30140
Delaefe <dimasdelafuen@gmail.com>
2016-10-06 15:30:14 +0200

    Setup XAMPP

--- controller/index.php
+++ controller/index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Casa Hernandez</title>
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
+
 	
 </body>
 </html>
