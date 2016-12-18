3d4251220bebf0e4be31cdfdbb2c9a069d5ce697
Dimas de la Fuente <dimasdelafuen@gmail.com>
2016-12-18 21:52:03 +0100

    Productos
    Estética lista, falta la BBDD y su implementación. Además de ordenar por
    categorías

--- view/productos.php
+++ view/productos.php
@@ -33,6 +33,103 @@
 				</div>
 			</section>
 
+			<section class="section-productos-wrap">
+				<div class="container">
+					<div class="productos-wrap">
+						<a href="" class="productos-item">
+							<div class="item">
+								<img src="../images/Productos/jamon.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</div>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</a>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/lomo.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/fuet.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/jamon.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/jamon.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/jamon.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/jamon.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/jamon.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+						<div class="productos-item">
+							<a href="" class="item">
+								<img src="../images/Productos/jamon.png" alt="jamon">
+									<div class="vermas">
+										<p>Ver más</p>
+									</div>
+							</a>
+							<h3>Jamón Ibérico de Bellota</h3>
+						</div>
+
+					</div>
+				</div>
+			</section>
+
 			
 			
 
